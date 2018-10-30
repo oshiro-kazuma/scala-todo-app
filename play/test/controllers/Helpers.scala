972a24cfe6847bba9ed1fc03b9f73d28b5ac96f2
oshiro-kazuma <kazuma.ooshiro@gmail.com>
2018-10-31 04:59:02 +0900

    StubRepository test

--- play/test/controllers/Helpers.scala
+++ play/test/controllers/Helpers.scala
@@ -1,12 +1,34 @@
 package controllers
 
+import models.Task
 import play.api.mvc._
+import repositories.TaskRepository
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class StubAuthAction(accountId: Int)(val parser: BodyParser[AnyContent])(implicit val executionContext: ExecutionContext) extends AuthAction {
   override def invokeBlock[A](request: Request[A], block: AuthRequest[A] => Future[Result]): Future[Result] = {
     block(new AuthRequest(accountId, request))
+  }
+}
+
+class StubTaskRepository(var data: Seq[Task]) extends TaskRepository {
+  override def all(): Future[Seq[Task]] = Future.successful(data)
+
+  override def find(id: Int): Future[Option[Task]] = Future.successful(data.find(_.id == id))
+
+  override def findByAccountId(accountId: Int): Future[Seq[Task]] = Future.successful(data.filter(_.accountId == accountId))
+
+  override def insert(task: Task): Future[Unit] = Future.successful {
+    if (data.exists(_.id == task.id)) throw new RuntimeException("Duplicate")
+  }
+
+  override def update(task: Task): Future[Unit] = Future.successful {
+    data = data.filterNot(_.id == task.id) :+ task
+  }
+
+  override def delete(id: Int): Future[Unit] = Future.successful {
+    data = data.filterNot(_.id == id)
   }
 }
 
--- play/test/controllers/TasksControllerSpec.scala
+++ play/test/controllers/TasksControllerSpec.scala
@@ -1,10 +1,10 @@
 package controllers
 
+import models.Task
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test.Helpers._
 import play.api.test._
-import repositories.TaskRepository
 
 import scala.concurrent.ExecutionContext
 
@@ -23,23 +23,25 @@ class TasksControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
     "/tasks" in {
       val controllerComponents = stubControllerComponents()
       val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
-      val controller = new TasksController(stubAuthAction, controllerComponents, inject[TaskRepository])
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task1", "Completed")))
+      val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
-      contentAsString(home) must include("Ok")
+      contentAsString(home) must include("""[{"id":1,"accountId":1,"name":"task1","status":"NotStarted"},{"id":2,"accountId":1,"name":"task1","status":"Completed"}]""")
     }
 
-    "/tasks/1" in {
+    "/tasks/2" in {
       val controllerComponents = stubControllerComponents()
       val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
-      val controller = new TasksController(stubAuthAction, controllerComponents, inject[TaskRepository])
-      val home = controller.show(11).apply(FakeRequest(GET, "/"))
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task2", "Completed")))
+      val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
+      val home = controller.show(2).apply(FakeRequest(GET, "/2"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
-      contentAsString(home) must include("""{"id":11,"accountId":1,"name":"xxx","status":"todxo"}""")
+      contentAsString(home) must include("""{"id":2,"accountId":1,"name":"task2","status":"Completed"}""")
     }
 
   }
