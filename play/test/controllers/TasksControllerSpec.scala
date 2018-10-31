6744df1298eb99095db4c066e1c6db1ab9139774
oshiro-kazuma <kazuma.ooshiro@gmail.com>
2018-10-31 19:05:30 +0900

    Add tasks controller test

--- play/test/controllers/TasksControllerSpec.scala
+++ play/test/controllers/TasksControllerSpec.scala
@@ -3,6 +3,7 @@ package controllers
 import models.Task
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test.Helpers._
 import play.api.test._
 
@@ -16,33 +17,79 @@ import scala.concurrent.ExecutionContext
   */
 class TasksControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
-  implicit val ec: ExecutionContext = ExecutionContext.Implicits.global
-
-  "TaskController GET" should {
-
-    "/tasks" in {
+  "GET /tasks" should {
+    "should response all my task" in {
       val controllerComponents = stubControllerComponents()
-      val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
-      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task1", "Completed")))
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+      val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task1", "Completed"), Task(3, 2, "other account task", "Completed")))
       val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+      val task = controller.index().apply(FakeRequest(GET, "/"))
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("application/json")
-      contentAsString(home) must include("""[{"id":1,"accountId":1,"name":"task1","status":"NotStarted"},{"id":2,"accountId":1,"name":"task1","status":"Completed"}]""")
+      status(task) mustBe OK
+      contentType(task) mustBe Some("application/json")
+      contentAsString(task) must include("""[{"id":1,"accountId":1,"name":"task1","status":"NotStarted"},{"id":2,"accountId":1,"name":"task1","status":"Completed"}]""")
     }
+  }
 
-    "/tasks/2" in {
+  "GET /tasks/:id" should {
+    "should response task id: 2" in {
       val controllerComponents = stubControllerComponents()
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+      val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task2", "Completed")))
+      val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
+      val task = controller.show(2).apply(FakeRequest(POST, "/"))
+
+      status(task) mustBe OK
+      contentType(task) mustBe Some("application/json")
+      contentAsString(task) must include("""{"id":2,"accountId":1,"name":"task2","status":"Completed"}""")
+    }
+  }
+
+  "POST /tasks" should {
+    "should create new task" in {
+      val controllerComponents = stubControllerComponents()
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
       val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
       val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task2", "Completed")))
       val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
-      val home = controller.show(2).apply(FakeRequest(GET, "/2"))
+      val home = controller.create().apply(FakeRequest(POST, "/").withHeaders("Content-type" -> "application/json").withBody(Json.parse("""{"name":"hoge","status": "todo"}""")))
+
+      status(home) mustBe CREATED
+      contentType(home) mustBe Some("application/json")
+      contentAsString(home) must include("Created")
+    }
+  }
+
+  "PUT /tasks/:id" should {
+    "should update task id: 2" in {
+      val controllerComponents = stubControllerComponents()
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+      val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task2", "Completed")))
+      val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
+      val home = controller.update(2).apply(FakeRequest(PUT, "/").withHeaders("Content-type" -> "application/json").withBody(Json.parse("""{"name":"changed","status": "Completed"}""")))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
-      contentAsString(home) must include("""{"id":2,"accountId":1,"name":"task2","status":"Completed"}""")
+      contentAsString(home) must include("Updated")
     }
-
   }
+
+  "DELETE /tasks/:id" should {
+    "should delete task id: 2" in {
+      val controllerComponents = stubControllerComponents()
+      implicit val ec: ExecutionContext = controllerComponents.executionContext
+      val stubAuthAction = new StubAuthAction(1)(controllerComponents.parsers.default)(controllerComponents.executionContext)
+      val taskRepository = new StubTaskRepository(Seq(Task(1, 1, "task1", "NotStarted"), Task(2, 1, "task2", "Completed")))
+      val controller = new TasksController(stubAuthAction, controllerComponents, taskRepository)
+      val home = controller.delete(2).apply(FakeRequest(DELETE, "/").withHeaders("Content-type" -> "application/json"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("application/json")
+      contentAsString(home) must include("Deleted")
+    }
+  }
+
 }
